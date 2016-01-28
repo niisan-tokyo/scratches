e69084f0de7e6a2c792dd50e052052af9f92833a
新倉23 <r-niikura@c-tos.com>
2016-01-28 20:35:41 +0900

    静的ファイルを置く

--- nodejs/koa_example/app.js
+++ nodejs/koa_example/app.js
@@ -5,6 +5,13 @@ const app = koa();
 
 require('./libs/render')(app);
 
+app.use(require('koa-static-server')({rootDir: 'assets', rootPath: '/assets'}));
+
+app.use(function *(next){
+  console.log('douda');
+  yield next;
+});
+
 app.use(require('./config/router')(app));
 
 app.listen(3000);
